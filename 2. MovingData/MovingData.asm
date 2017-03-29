815a5a0d0d87c08bf75fbb0e27e2deb115d442d6
abatchy <abatchy@abatchy.com>
2017-03-28 22:25:24 -0700

    Update MovingData.asm

--- 2. MovingData/MovingData.asm
+++ 2. MovingData/MovingData.asm
@@ -12,16 +12,15 @@ _start:
 	mov eax, 0xdeadbeef	; eax = 0xdeadbeef
 	mov ebx, 0xaabbccdd	; ebx = 0xaabbccdd
 
-	xchg eax, ebx			; eax = 0xaabbccdd
-						; ebx = 0xdeadbeef
+	xchg eax, ebx		; eax = 0xaabbccdd, ebx = 0xdeadbeef
 
 	lea eax, [var]		; eax = effective address of var
 	lea ebx, [eax]		; ebx = effective address of var (which matches eax)
 
 	; size directive is needed, otherwise assember won't know which interpretation is valid
 	
-	mov byte [var], 5		; var: 0x05 0xbb 0xcc 0xdd 0xee 0xff 0x11 0x22
-	mov word [var], 5		; var: 0x0005 0xddcc 0xffee 0x2211
+	mov byte [var], 5	; var: 0x05 0xbb 0xcc 0xdd 0xee 0xff 0x11 0x22
+	mov word [var], 5	; var: 0x0005 0xddcc 0xffee 0x2211
 	mov dword [var], 5	; var: 0x00000005 0x2211ffee
 
 	; exit program
